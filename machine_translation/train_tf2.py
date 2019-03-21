810a95fe3e99beda6851a5a2dd42879a98cc0ca0
Trung Tran <trungtran@machinetalk.org>
2019-03-21 04:29:57 +0000

    Add source to train eng->fra translation model

--- machine_translation/train_tf2.py
+++ machine_translation/train_tf2.py
@@ -1,8 +1,22 @@
-import tensorflow as tf
-import numpy as np
-import unicodedata
-import re
+# -*- coding: utf-8 -*-
 
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+import re
+import unicodedata
+
+with open('../data/eng_fra.txt') as f:
+    lines = f.read()
+
+raw_data = []
+for line in lines.split('\n'):
+    raw_data.append(line.split('\t'))
+
+print(raw_data[-5:])
+raw_data = raw_data[:-1]
+
+'''
 raw_data = (
     ('What a ridiculous concept!', 'Quel concept ridicule !'),
     ('What about his girlfriend?', "Qu'en est-il de sa copine ?"),
@@ -13,28 +27,26 @@ raw_data = (
     ('Who else knows the answer?', "Qui d'autre connaît la réponse ?"),
     ('He got up earlier than usual.', "Il s'est levé plus tôt que d'habitude."),
     ('He is the oldest of them all.', "C'est le plus vieux d'entre eux."),
-    ('He left school two weeks ago.',
-     "Il a obtenu son diplôme de fin d'année il y a 2 semaines."),
+    ('He left school two weeks ago.', "Il a obtenu son diplôme de fin d'année il y a 2 semaines."),
     ('He reached Kyoto on Saturday.', "Il est arrivé samedi à Kyoto."),
     ('He refused my friend request.', "Il a refusé ma demande pour devenir amis."),
     ('He refused to take the bribe.', "Il s'est refusé à prendre le pot-de-vin."),
-    ('He studied the way birds fly.',
-     "Il étudiait la manière dont les oiseaux volent."),
+    ('He studied the way birds fly.', "Il étudiait la manière dont les oiseaux volent."),
     ('He wondered why she did that.', "Il se demanda pourquoi elle avait fait cela."),
     ('Health is better than wealth.', "La santé est plus importante que la richesse."),
     ('Her face suddenly turned red.', "Son visage vira soudain au rouge."),
-    ('His ideas conflict with mine.',
-     "Ses idées rentrent en conflit avec les miennes."),
+    ('His ideas conflict with mine.', "Ses idées rentrent en conflit avec les miennes."),
     ('I advised Tom not to do that.', "J'ai conseillé à Tom de ne pas faire cela."),
-    ("I didn't know you were that good at French.",
-     "J'ignorais que vous étiez aussi bonnes en français.")
+    ("I didn't know you were that good at French.", "J'ignorais que vous étiez aussi bonnes en français.")
 )
+'''
 
 
 def unicode_to_ascii(s):
     return ''.join(
         c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn')
+        if unicodedata.category(c) != 'Mn'
+    )
 
 
 def normalize_string(s):
@@ -45,17 +57,20 @@ def normalize_string(s):
     return s
 
 
-raw_data_en, raw_data_fr = list(zip(*raw_data))
-raw_data_en, raw_data_fr = list(raw_data_en), list(raw_data_fr)
+raw_data_en = list(map(lambda x: x[0], raw_data))
+raw_data_fr = list(map(lambda x: x[1], raw_data))
 raw_data_en = ['<start> ' + data + ' <end>' for data in raw_data_en]
 raw_data_fr_in = ['<start> ' + data for data in raw_data_fr]
 raw_data_fr_out = [data + ' <end>' for data in raw_data_fr]
+
+print(raw_data_fr_out[-5:])
 
 en_tokenizer = tf.keras.preprocessing.text.Tokenizer(1000, filters='')
 en_tokenizer.fit_on_texts(raw_data_en)
 data_en = en_tokenizer.texts_to_sequences(raw_data_en)
 data_en = tf.keras.preprocessing.sequence.pad_sequences(data_en,
                                                         padding='post')
+print(data_en[:2])
 
 fr_tokenizer = tf.keras.preprocessing.text.Tokenizer(1000, filters='')
 fr_tokenizer.fit_on_texts(raw_data_fr_in)
@@ -63,14 +78,21 @@ fr_tokenizer.fit_on_texts(raw_data_fr_out)
 data_fr_in = fr_tokenizer.texts_to_sequences(raw_data_fr_in)
 data_fr_in = tf.keras.preprocessing.sequence.pad_sequences(data_fr_in,
                                                            padding='post')
+print(data_fr_in[:2])
 
 data_fr_out = fr_tokenizer.texts_to_sequences(raw_data_fr_out)
 data_fr_out = tf.keras.preprocessing.sequence.pad_sequences(data_fr_out,
                                                             padding='post')
+print(data_fr_out[:2])
+
+BATCH_SIZE = 128
+EMBEDDING_SIZE = 256
+LSTM_SIZE = 512
 
 dataset = tf.data.Dataset.from_tensor_slices(
     (data_en, data_fr_in, data_fr_out))
-dataset = dataset.shuffle(20).batch(5)
+dataset = dataset.shuffle(len(raw_data_en)).batch(
+    BATCH_SIZE, drop_remainder=True)
 
 
 class Encoder(tf.keras.Model):
@@ -93,10 +115,13 @@ class Encoder(tf.keras.Model):
 
 
 en_vocab_size = len(en_tokenizer.word_index) + 1
-EMBEDDING_SIZE = 32
-LSTM_SIZE = 64
 
 encoder = Encoder(en_vocab_size, EMBEDDING_SIZE, LSTM_SIZE)
+
+initial_state = encoder.init_states(1)
+test_encoder_output = encoder(tf.constant(
+    [[1, 23, 4, 5, 0, 0]]), initial_state)
+print(test_encoder_output[0].shape)
 
 
 class Decoder(tf.keras.Model):
@@ -122,6 +147,11 @@ class Decoder(tf.keras.Model):
 
 fr_vocab_size = len(fr_tokenizer.word_index) + 1
 decoder = Decoder(fr_vocab_size, EMBEDDING_SIZE, LSTM_SIZE)
+de_initial_state = test_encoder_output[1:]
+
+test_decoder_output = decoder(tf.constant(
+    [[1, 3, 5, 7, 9, 0, 0, 0]]), de_initial_state)
+print(test_decoder_output[0].shape)
 
 
 def loss_func(targets, logits):
@@ -156,18 +186,50 @@ def train_step(source_seq, target_seq_in, target_seq_out, en_initial_states):
     return loss
 
 
-NUM_EPOCHS = 300
-BATCH_SIZE = 5
+def predict():
+    test_source_text = raw_data_en[np.random.choice(len(raw_data_en))]
+    print(test_source_text)
+    test_source_seq = en_tokenizer.texts_to_sequences([test_source_text])
+    print(test_source_seq)
+
+    en_initial_states = encoder.init_states(1)
+    en_outputs = encoder(tf.constant(test_source_seq), en_initial_states)
+
+    de_input = tf.constant([[fr_tokenizer.word_index['<start>']]])
+    de_state_h, de_state_c = en_outputs[1:]
+    out_words = []
+
+    while True:
+        de_output, de_state_h, de_state_c = decoder(
+            de_input, (de_state_h, de_state_c))
+        de_input = tf.argmax(de_output, -1)
+        out_words.append(fr_tokenizer.index_word[de_input.numpy()[0][0]])
+
+        if out_words[-1] == '<end>' or len(out_words) >= 20:
+            break
+
+    print(' '.join(out_words))
+
+
+NUM_EPOCHS = 30
+
+encoder.load_weights('checkpoints/encoder_30.h5')
+decoder.load_weights('checkpoints/decoder_30.h5')
 
 for e in range(NUM_EPOCHS):
+    en_initial_states = encoder.init_states(BATCH_SIZE)
+    encoder.save_weights('checkpoints/encoder_{}.h5'.format(e + 1))
+    decoder.save_weights('checkpoints/decoder_{}.h5'.format(e + 1))
+
+    predict()
+
     for batch, (source_seq, target_seq_in, target_seq_out) in enumerate(dataset.take(-1)):
-        en_initial_states = encoder.init_states(BATCH_SIZE)
-        loss = train_step(source_seq, target_seq_in,
-                          target_seq_out, en_initial_states)
+        loss = train_step(source_seq, target_seq_in, target_seq_out, en_initial_states)
+        
+        if batch % 100 == 0:
+            print('Epoch {} Batch {} Loss {:.4f}'.format(e + 1, batch, loss.numpy()))
 
-    print('Epoch {} Loss {:.4f}'.format(e + 1, loss.numpy()))
-
-test_source_text = raw_data_en[np.random.choice(20)]
+test_source_text = raw_data_en[np.random.choice(len(raw_data_en))]
 print(test_source_text)
 test_source_seq = en_tokenizer.texts_to_sequences([test_source_text])
 print(test_source_seq)
@@ -185,7 +247,7 @@ while True:
     de_input = tf.argmax(de_output, -1)
     out_words.append(fr_tokenizer.index_word[de_input.numpy()[0][0]])
 
-    if out_words[-1] == '<end>':
+    if out_words[-1] == '<end>' or len(out_words) >= 20:
         break
 
 print(' '.join(out_words))
