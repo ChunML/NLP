9bf0fd39583289354f30f04e00e4ee3014ee4713
Trung Tran <trungtran.chunml@gmail.com>
2019-06-25 18:20:00 +0900

    Update source to train a chatbot with Cornell movie dataset

--- chatbot/process_cornell.py
+++ chatbot/process_cornell.py
@@ -3,7 +3,7 @@ import itertools
 from collections import Counter
 import re
 
-rootpath = '../data/cornell'
+rootpath = './cornell movie-dialogs corpus'
 movie_conversations = os.path.join(rootpath, 'movie_conversations.txt')
 movie_lines = os.path.join(rootpath, 'movie_lines.txt')
 
@@ -16,6 +16,15 @@ def process_line(line):
 
 
 def create_training_files():
+    input_text_file = 'processed_input_data.txt'
+    target_text_file = 'processed_target_data.txt'
+    vocab_text_file = 'vocab.txt'
+
+    if (os.path.exists(input_text_file) and
+        os.path.exists(target_text_file) and
+        os.path.exists(vocab_text_file)):
+        return input_text_file, target_text_file, vocab_text_file
+
     line_dict = {}
     words = []
     with open(movie_lines, 'r', encoding='iso-8859-1') as f:
@@ -37,22 +46,23 @@ def create_training_files():
                 input_sequence.append(line_dict[line_ids[i]])
                 target_sequence.append(line_dict[line_ids[i + 1]])
 
-    with open('processed_input_data.txt', 'w', encoding='utf-8') as f:
+    with open(input_text_file, 'w', encoding='utf-8') as f:
         for sent in input_sequence:
             f.write(sent)
             f.write('\n')
 
-    with open('processed_target_data.txt', 'w', encoding='utf-8') as f:
+    with open(target_text_file, 'w', encoding='utf-8') as f:
         for sent in target_sequence:
             f.write(sent)
             f.write('\n')
 
     words = list(itertools.chain(*words))
     word_counts = Counter(words)
-    word_counts = [k for (k, v) in dict(filter(lambda x: x[1] > 10, tuple(word_counts.items()))).items()]
+    word_counts = [k for (k, v) in dict(
+        filter(lambda x: x[1] > 10, tuple(word_counts.items()))).items()]
     print('Number of unique words:', len(word_counts))
 
-    with open('vocab.txt', 'w', encoding='utf-8') as f:
+    with open(vocab_text_file, 'w', encoding='utf-8') as f:
         f.write('<unk>\n')
         f.write('<sos>\n')
         f.write('<eos>\n')
@@ -60,7 +70,8 @@ def create_training_files():
             f.write(word)
             f.write('\n')
 
+    return input_text_file, target_text_file, vocab_text_file
+
+
 if __name__ == '__main__':
     create_training_files()
-
-
