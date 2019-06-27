1ee589c84fdb4caf2936ef24330d8cbb10e06290
Trung Tran <trungtran.chunml@gmail.com>
2019-06-27 17:56:14 +0900

    Update test logic

--- chatbot/test.py
+++ chatbot/test.py
@@ -1,5 +1,9 @@
 import numpy as np
 import tensorflow as tf
+import argparse
+import yaml
+from data import create_dataset
+from model import create_transformer
 
 
 def predict(encoder, decoder, tokenizer, raw_input_lines, max_length):
@@ -16,9 +20,9 @@ def predict(encoder, decoder, tokenizer, raw_input_lines, max_length):
         The output string array
     """
     test_source_text = np.random.choice(raw_input_lines)
-    print(test_source_text)
+    # print(test_source_text)
     test_source_seq = tokenizer.texts_to_sequences([test_source_text])
-    print(test_source_seq)
+    # print(test_source_seq)
 
     en_output, en_alignments = encoder(
         tf.constant(test_source_seq), training=False)
@@ -42,5 +46,40 @@ def predict(encoder, decoder, tokenizer, raw_input_lines, max_length):
         if out_words[-1] == '<end>' or len(out_words) >= max_length:
             break
 
-    print(' '.join(out_words))
+    print('Bot: ' + ' '.join(out_words))
     return en_alignments, de_bot_alignments, de_mid_alignments, test_source_text.split(' '), out_words
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_length', default=30, type=int,
+                        help='maximum length of training sentences')
+    parser.add_argument('--config', default='./config/base.yml',
+                        help='config file for Transformer')
+    parser.add_argument('--encoder_weights_path', required=True,
+                        help='path to encoder weights')
+    parser.add_argument('--decoder_weights_path', required=True,
+                        help='path to decoder weights')
+    args = parser.parse_args()
+
+    with open(args.config, 'r') as f:
+        config = yaml.load(f, yaml.SafeLoader)
+
+    # TODO: Is there any smarter way to get the vocab size?
+    _, info = create_dataset(
+        args.max_length, 1, 100)
+
+    encoder, decoder = create_transformer(
+        info['vocab_size'], config['MODEL_SIZE'],
+        info['max_length'], config['NUM_LAYERS'], config['H'])
+
+    encoder.load_weights(args.encoder_weights_path)
+    decoder.load_weights(args.decoder_weights_path)
+
+    input_sentence = [input('Chun: ')]
+
+    while input_sentence[0] != 'quit':
+        predict(encoder, decoder, info['tokenizer'],
+                input_sentence, info['max_length'])
+
+        input_sentence = [input('Chun: ')]
