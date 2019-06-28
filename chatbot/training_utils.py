bf147179af2172eb239b18603b7bd09ec8264721
Trung Tran <trungtran.chunml@gmail.com>
2019-06-28 10:56:43 +0900

    Update training_utils.py

--- chatbot/training_utils.py
+++ chatbot/training_utils.py
@@ -21,16 +21,17 @@ class WarmupThenDecaySchedule(tf.keras.optimizers.schedules.LearningRateSchedule
         warmup_steps: number of warmup steps at the beginning
     """
 
-    def __init__(self, model_size, warmup_steps=4000, trained_step=43000):
+    def __init__(self, model_size, warmup_steps=4000, trained_steps=43000):
         super(WarmupThenDecaySchedule, self).__init__()
-
+        
+        self.trained_steps = trained_steps
         self.model_size = model_size
         self.model_size = tf.cast(self.model_size, tf.float32)
 
         self.warmup_steps = warmup_steps
 
     def __call__(self, step):
-        step_term = tf.math.rsqrt(step + trained_step)
+        step_term = tf.math.rsqrt(step + self.trained_steps)
         warmup_term = step * (self.warmup_steps ** -1.5)
 
         return 0.05 * tf.math.minimum(step_term, warmup_term)
